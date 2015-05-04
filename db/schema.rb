3c0e1d6696333b7c5adc0faf61ac24dc386eb331
Kevin <supergoodblog@gmail.com>
2015-05-04 02:41:44 -0500

    added working username attribute to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430062808) do
+ActiveRecord::Schema.define(version: 20150504073049) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "username"
   end
 
 end
